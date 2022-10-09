dfa55b9374eecc57e42e0aaac53d1d24251becba
RaziaSana-1 <raziasana2020@gmail.com>
2022-10-09 22:44:18 +0430

    oop fifth file added

---
+++ oop-fifth.js
@@ -0,0 +1,22 @@
+console.log('hello world')
+var firstname = 'johan';
+console.log(firstname)
+var age = 22;
+console.log(age)
+var fullage=true;
+console.log(fullage)
+var job;
+console.log(job)
+
+var firstnamee, agee;
+firstnamee= 'khan';
+agee=22;
+console.log(firstnamee+' '+agee)
+
+agee='twenty';
+firstnamee='ahmad';
+
+var ahmadage = 22-20;
+console.log(ahmadage)
+console.log(typeof ahmadage)
+
