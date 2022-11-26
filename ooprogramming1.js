7c723e786bf0a80496049feb7bba2f8a66102334
Razia <103430503+RaziaSana-1@users.noreply.github.com>
2022-11-26 21:51:59 +0430

    Delete ooprogramming1.js

--- ooprogramming1.js
+++
@@ -1,9 +0,0 @@
-var alexMass = 60;
-var alexhight = 5.6;
-var ahmadmass = 50;
-var ahmadhight = 4.10;
-var alexBMI = alexMass/(alexhight*alexhight);
-var ahmadBMI = ahmadmass/(ahmadhight*ahmadhight);
-console.log( alexBMI, ahmadBMI)
-var higherBMI = alexBMI>ahmadBMI;
-console.log('is alexBMI higher than ahmadBMI' + higherBMI)
